4950167bb553bf9b64656c4c1bddb756ea1059eb
hjames034 <60247356+hjames034@users.noreply.github.com>
2022-06-09 22:26:24 -0400

    Update shakeoutStat.py

--- shakeoutStat.py
+++ shakeoutStat.py
@@ -57,7 +57,7 @@ for region in cenList:
             try:
                 if len(tmp_list) > 1 and tmp_list[0] in cols:
                     dictionaryReturn[tmp_list[0]]=[tmp_list[1]]
-                    catSpecific[region]={tmp_list[0]]:tmp_list[1]}
+                    catSpecific[region]={tmp_list[0]:tmp_list[1]}
                     dictFind[region]={row:tmp_list[1]}
                     #colUnused.remove(tmp_list[0])
                 else:
