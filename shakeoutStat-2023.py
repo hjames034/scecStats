3a4a792ce616b25653a9783a3e44bd27ffbbfb6c
hjames034 <60247356+hjames034@users.noreply.github.com>
2023-03-20 12:05:33 -0700

    Update shakeoutStat-2023.py

--- shakeoutStat-2023.py
+++ shakeoutStat-2023.py
@@ -17,7 +17,7 @@ from datetime import date,timedelta,datetime
 
 today = (datetime.now() - timedelta(days = 1 )).date()
 
-driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM,version="109.0.5414.74").install()
+driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM,version="111.0.5563.64").install()
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
